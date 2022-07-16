62e8fcdbe9c099b5bdbcdfb1fbdcc9e816b6f511
Anurag-Bharati <anuragbharati26@gmail.com>
2022-07-16 17:46:05 +0545

    Gathered and crafted relevant district pcd | Added helper functions

--- src/00 Setup/00 Setup.R
+++ src/00 Setup/00 Setup.R
@@ -3,4 +3,11 @@
 if(!"tidyverse" %in% (.packages())){library(tidyverse,warn.conflicts = F)} else {print("Tidyverse has already been loaded.")}
 if(!"lubridate" %in% (.packages())){library(lubridate,warn.conflicts = F)} else {print("Lubridate has already been loaded.")}
 
-# TODO collect relevant
+# CORE COMPONENT ----
+MerseysidePCD <- read_csv("./data/mydata/PostalCodeToDistrictMerseyside.csv", show_col_types = F)
+ManchesterPCD <- read_csv("./data/mydata/PostalCodeToDistrictManchester.csv", show_col_types = F)
+ReleventPCD = ManchesterPCD %>%
+  mutate(County="GREATER MANCHESTER") %>%
+  add_row(MerseysidePCD%>%mutate(County="MERSEYSIDE")) %>%
+  mutate(County = as.factor(County), District = as.factor(District))
+rm(ManchesterPCD, MerseysidePCD)
--- src/00 Setup/01 Functions.R
+++ src/00 Setup/01 Functions.R
@@ -1,2 +1,16 @@
 # HELPER FUNCTIONS ----
 normalize <- function(x)((x-min(x))/(max(x)-min(x)))
+# Overview of missing value in percentage (col wise)
+naCols <-  function (df) sapply(df, function(x) sum(x=="" | is.na(x))/nrow(df) * 100)
+# Overview of missing value in percentage (row wise)
+naRows <-  function (df, show=10, rtn=F){
+  a <- df %>% apply(1, function(x) sum(x==""| is.na(x))/ncol(df) * 100)
+  if (!rtn) return(a %>% sort(., decreasing = T) %>% head(show))
+  else return(a)
+}
+percentDuplicate <- function(x){
+  c <- count(x)
+  dc <- count(distinct(x))
+  return (as.numeric((c - dc)/c*100))
+  rn(c, dc)
+}
